71cdccdd7bb038f8f31601001ee57620873be753
c4cat <313187850@qq.com>
2014-06-14 18:03:19 +0800

    Signed-off-by: c4cat <313187850@qq.com>

--- static/js/drag.js
+++ static/js/drag.js
@@ -6,21 +6,66 @@
 $(function(){
 	$('.drag').draggable({
 		create:function(event,ui){
-			// console.log('fxxk');
+
 		},
 		start:function(event,ui){
-			this.x = event.clientX;
-			$(this).addClass('rotate-left');
-			console.log($(this));
-			console.log(event.clientX);
+			this.clientX_start = event.clientX;
+			$(this).addClass('rotate-go-right');
+			// console.log($(this));
+			console.log('start');
+
+			// $(this).mousedown(function(){
+				console.log('mousedown' + event.clientX);
+			// })
+			// console.log(event.clientX);
 		},
 		drag:function(event,ui){
-			// console.log(ui);
-			console.log(event.clientX);
+			//console.log(event);
+			//console.log(event.clientX);
+		 	this.offsetX = event.offsetX;
+		 	this.offsetY = event.offsetY;
+		 	// console.log(event);
+		 	// console.log(this.offsetX);
+		 	if(event.clientX + 10 > $(window).width()){
+		 		return false;
+		 	}
 		},
 		stop:function(event,ui){
-			$(this).removeClass('rotate-left');
-		}
+			this.clientX_end = 
+			$(this).removeClass('rotate-go-right');
+			$(this).addClass('crashing');
+				var offsetX  = event.offsetX - this.offsetX;
+				var offsetY  = event.offsetY - this.offsetY;
+
+				console.log(event.clientX);
+
+				var obj = $('#drag');
+
+				crash(event.clientX,event.clientY);
+		},
 	});
+
+	console.log(document.documentElement.clientHeight);
+	console.log($(window).width());
+
+	function crash(clientX,clientY){
+		var window_width = $(window).width(),
+			window_height = $(window).height(),
+			i;
+
+			if(window_width - clientX){
+
+			}
+			// setInterval(function(){
+				clientX += 100;
+				// $('.crashing').css('left',clientX+'px');
+				  $('.crashing').animate({'left': window_width - 150 +'px','top':'150px'}, 200, function(){
+				  		$('.crashing').animate({'left': '500px'},100);
+				  })
+			// },50);
+			// setTimeout(crash,30);
+
+	};
+
 });
 

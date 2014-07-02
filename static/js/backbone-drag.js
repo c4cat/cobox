05d8bf591b2e81c9e6f4dd62eedbd6847c700272
mrc <313187850@qq.com>
2014-07-02 23:51:52 +0800

    2014年07月02日23:51:50
    Signed-off-by: mrc <313187850@qq.com>

--- static/js/backbone-drag.js
+++ static/js/backbone-drag.js
@@ -16,41 +16,18 @@ function get_wh(){
 	return re;
 }
 //set 
-function set_wh(arg){
-	$(arg).css({'width':get_wh().w,'height':get_wh().h});
-}
+// function set_wh(arg){
+// 	$(arg).css({'width':get_wh().w,'height':get_wh().h});
+// }
 
-function set_img_wh(arg){
-	$(arg).css({'width':get_wh().w,'height':'auto'});
-}
+// function set_img_wh(arg){
+// 	$(arg).css({'width':get_wh().w,'min-height':get_wh().h});
+// }
 
 // jquery 
 $(function(){
 
-// .box model
-var BgBox = Backbone.Model.extend({
-	initialize:function(){
-		// this.listenTo(this,'change',this.test);
-		// this.bind("change:test",function(){
-		// 	alert('123');
-		// });
-		console.log(123);
-	},
-
-	test:function(){
-		alert('have a test');
-	}
-});
-
-// .box collection 
-// no need, maybe
-// var BgBoxList = Backbone.Collection.extend({
-// 	model: Bgbox,
-// 	initialize:function(){
-
-// 	},
-// 	comparator: 'order'
-// });
+//start bg view
 
 var BgView = Backbone.View.extend({
 	// 
@@ -93,8 +70,7 @@ var BgView = Backbone.View.extend({
 		}
 
 		$('#bg-sp').css('left',offect);
-		$('#bg-img>img').css({'height':'auto','width':get_wh().w});
-		// console.log($('.box').length);		
+		$('#bg-img>img').css({'width':get_wh().w,'min-height':get_wh().h});
 	},
 	removee:function(){
 		$('#bg-nm,#bg-sp').empty();
@@ -102,13 +78,40 @@ var BgView = Backbone.View.extend({
 	setBgImage:function(){
 		var arr = ['./static/img/b1.jpg','./static/img/b2.jpg','./static/img/b3.jpg','./static/img/b4.jpg','./static/img/b5.jpg','./static/img/b6.jpg','./static/img/b7.jpg','./static/img/b8.jpg','./static/img/b9.jpg','./static/img/b10.jpg','./static/img/b11.jpg'],
 			random = _.random(0,10);
-		// $('#bg-img>img').attr('src',arr[random]);	  
-		$('#bg-img>img').attr('src','./static/img/b2.jpg');	  
+		$('#bg-img>img').attr('src',arr[random]);	  
+		// $('#bg-img>img').attr('src','./static/img/b2.jpg');	  
 	}
 	});
 
+//  end Bgview;
+
+// .box model
+var Box = Backbone.Model.extend({
+	initialize:function(){
+		this.add();
+		console.log(123);
+	},
+	addOne:fnction(){
+		$('#region').append("<div class='box'></div>");
+	},
+	add:function(){
+
+	},
+	test:function(){
+		alert('have a test');
+	}
+});
+
+// .box collection 
+// var BgBoxList = Backbone.Collection.extend({
+// 	model: Bgbox,
+// 	initialize:function(){
+
+// 	},
+// 	comparator: 'order'
+// });
+
 var app = new BgView();
 
-//bg.set({test:'123'});
 
 });
